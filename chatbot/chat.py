e6d823e97d5f3070cae95e85326c4cc148069d1b
freedisch <freeproduc@gmail.com>
2024-06-25 19:58:54 +0200

    Hot fix
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -20,6 +20,36 @@ classes=pickle.load(open('Classes.pkl','rb'))
 model=load_model('ChatbotModel.h5')
 
 correctQues = ""
+def minDis(s1, s2, n, m, dp):
+    if n == 0:
+        return m
+    if m == 0:
+        return n
+    if dp[n][m] != -1:
+        return dp[n][m]
+    if s1[n - 1] == s2[m - 1]:
+        if dp[n - 1][m - 1] == -1:
+            dp[n][m] = minDis(s1, s2, n - 1, m - 1, dp)
+            return dp[n][m]
+        else:
+            dp[n][m] = dp[n - 1][m - 1]
+            return dp[n][m]
+    else:
+        if dp[n - 1][m] != -1:
+            m1 = dp[n - 1][m]
+        else:
+            m1 = minDis(s1, s2, n - 1, m, dp)
+        if dp[n][m - 1] != -1:
+            m2 = dp[n][m - 1]
+        else:
+            m2 = minDis(s1, s2, n, m - 1, dp)
+        if dp[n - 1][m - 1] != -1:
+            m3 = dp[n - 1][m - 1]
+        else:
+            m3 = minDis(s1, s2, n - 1, m - 1, dp)
+        dp[n][m] = 1 + min(m1, min(m2, m3))
+        return dp[n][m]
+    
 def correct_spelling(sentence):
   sentence_list = []
   for sent in sentence.split():
@@ -29,7 +59,7 @@ def correct_spelling(sentence):
         n = len(sent)
         m = len(w)
         dp = [[-1 for i in range(m + 1)] for j in range(n + 1)]
-        diff = chat.minDis(sent, w, n, m, dp)
+        diff = minDis(sent, w, n, m, dp)
         if dist > diff:
             dist = diff
             word = w
